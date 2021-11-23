00262f1c500542d7d8d7f3e829899461fd6d5c74
Hotchand <Hotchand@users.noreply.github.com>
2021-11-23 18:50:43 +0500

    Create registration.php

---
+++ RedHunt/AlwaysShip/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © RedHunt All rights reserved.
+ * See COPYING.txt for license details.
+ * contact: hotchand.sajnani@gmail.com
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'RedHunt_AlwaysShip', __DIR__);
+
